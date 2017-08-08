0418feb7db3e9e6a73b86bfe2f949a561885bdd8
Tomáš Hodbod <tom@sayahoyahoy.com>
2017-08-08 15:40:30 +0200

    git flow

--- run.sh
+++ run.sh
@@ -23,13 +23,15 @@ fi
 
 rm -rf .git
 git init
-git config --global user.email "werckerbot@purple-technology.com"
-git config --global user.name "werckerbot"
+git config user.email "werckerbot@purple-technology.com"
+git config user.name "werckerbot"
+git checkout master
 git add --all .
 git commit -am "[ci skip] deploy from $WERCKER_STARTED_BY"
+git show-ref
 
 L=/usr/local/bin/flynn && curl -sSL -A "`uname -sp`" https://dl.flynn.io/cli | zcat >$L && chmod +x $L
 flynn cluster add $FLYNN_CLUSTER_NAME $FLYNN_CONTROLLER_DOMAIN $FLYNN_CONTROLLER_KEY
 flynn -a $FLYNN_APP_NAME remote add
 
-git push -f flynn HEAD:master
+git push -f flynn master
