a4bb44f4c91cc279423a1bf1a27bc29c76ea17a4
Tomáš Hodbod <tom@sayahoyahoy.com>
2017-08-08 13:01:10 +0200

    echo test

--- run.sh
+++ run.sh
@@ -25,6 +25,9 @@ if [ ! -n "$FLYNN_APP_NAME" ]; then
     exit 1
 fi
 
+echo $FLYNN_CLUSTER_NAME
+echo $FLYNN_CONTROLLER_DOMAIN
+echo $FLYNN_APP_NAME
 L=/usr/local/bin/flynn && curl -sSL -A "`uname -sp`" https://dl.flynn.io/cli | zcat >$L && chmod +x $L
 flynn cluster add -p $FLYNN_TLSPIN $FLYNN_CLUSTER_NAME $FLYNN_CONTROLLER_DOMAIN $FLYNN_CONTROLLER_KEY
 flynn -a $FLYNN_APP_NAME remote add
